73a0a5b98c24d57f542042fab72696cb369e6567
Kent C. Dodds <me+github@kentcdodds.com>
2025-05-05 17:18:51 -0600

    progress maybe

--- exercises/07.error-boundaries/03.solution.not-found/vite.config.ts
+++ exercises/07.error-boundaries/03.solution.not-found/vite.config.ts
@@ -1,10 +1,11 @@
+import { reactRouter } from '@react-router/dev/vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [react(), tailwindcss(), reactRouter()],
 	server: {
 		port: process.env.PORT ? Number(process.env.PORT) : undefined,
 	},
